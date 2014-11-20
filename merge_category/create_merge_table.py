c8a1c58c8a18f8c355f0be760144cdd428da974a
Roy Xue <xljroy@gmail.com>
2014-11-21 00:59:44 +0800

    Start merge categories process

---
+++ merge_category/create_merge_table.py
@@ -0,0 +1,30 @@
+"""
+Scripts for create a new table to store the merge category information
+
+reference: category_merge_plan.txt
+"""
+
+import MySQLdb
+
+def create_merge_table():
+    connection = MySQLdb.connect('localhost', 'root', '', 'mobile_analysis')
+    t = connection.cursor()
+
+    category_list =  ['Unknown', 'Communication', 'Social', 'Tools', 'Travel', 'Productivity', 'Weather', 'News', 'Media', 'Photography', 'Finance', 'Entertainment', 'Books', 'Personalization', 'Lifestyle', 'Music', 'Game']
+    time_of_day = ['morning', 'afternoon', 'evening']
+
+    # Create new table
+    for category in category_list:
+        for time in time_of_day:
+            column_name = category + '_' + time
+            sql = "ALTER TABLE merge_result ADD %s float DEFAULT '0'" % (column_name)
+            t.execute(sql)
+
+    connection.commit()
+
+if __name__ == '__main__':
+    create_merge_table()
+
+
+    
+
