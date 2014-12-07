fb6520629f2e47d7e293eba79b5f86b453de4054
Roy Xue <xljroy@gmail.com>
2014-12-07 10:42:47 +0800

    Change folder names

---
+++ old_kmeans/gap_static.r
@@ -0,0 +1,26 @@
+# Start from 2 to 20
+begin = 2; 
+count = 50;
+end = 20;
+ 
+# Store result
+result = c();
+result[begin:end] = -1;
+ 
+library(cluster);
+qc = read.table("/Users/royxue/Code/CMU_Projects/code/cluster/input_data.txt");
+for(i in begin:end) {
+    # Silhouette coefficient
+    tmp = c();
+    tmp[1:count] = 0;
+    for(j in 1:count) {
+        gap_stat = clusGap(qc, FUN=pam, K.max=i);
+        tmp[j] = kcluster$silinfo$avg.width;
+    }
+    result[i] = mean(tmp);
+	#result[i] = sd(tmp, na.rm=FALSE);
+}
+ 
+
+plot(result, type="o", xlab="Number of Cluster", ylab="Silhouette Coefficient");
+
