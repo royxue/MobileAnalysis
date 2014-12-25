ffd66b5f4ff9ccfdf9da757bed545454beab2ab9
Roy Xue <xljroy@gmail.com>
2014-12-25 15:17:33 +0800

    Update new graphs

--- heat_map/hclust_heatmap.R
+++ heat_map/hclust_heatmap.R
@@ -4,4 +4,4 @@ l_data = data[,3:47];
 dist <- dist(l_data,method="euclidean");
 hclust <- hclust(dist,method="complete");
 plot(hclust);
-heatmap(as.matrix(dist),labRow = F, labCol = F)
+heatmap(as.matrix(dist))
